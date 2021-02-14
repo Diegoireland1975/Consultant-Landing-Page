3d8d43db5d200b33592602058644968b33f1b78d
Diego Raffa <diego170975@gmail.com>
2021-02-14 15:44:51 +0000

    added collapsible banner on click

--- js/main.js
+++ js/main.js
@@ -1,8 +1,37 @@
-const panel = document.querySelectorAll(".question-card-header");
-const show = document.getElementsByClassName("question-card-body");
+// const panel = document.querySelectorAll(".question-card-header");
+// const show = document.getElementsByClassName("question-card-body");
 
-panels.forEach((panel) => {
-  panel.addEventListener("click", () => {
-    show.classList.toggle("show");
-  });
+// panels.forEach((panel) => {
+//   panel.addEventListener("click", () => {
+//     show.classList.toggle("show");
+//   });
+// });
+
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible-expanded");
+  })
+);
+// ##########
+// LOAD MORE
+// ##########
+
+const loadmore = document.querySelector("#loadmore");
+let currentItems = 2;
+loadmore.addEventListener("click", (e) => {
+  const elementList = [
+    ...document.querySelectorAll(".question-card .question-card-header"),
+  ];
+  for (let i = currentItems; i < currentItems + 2; i++) {
+    if (elementList[i]) {
+      elementList[i].style.display = "block";
+    }
+  }
+  currentItems += 2;
+
+  // Load more button will be hidden after list fully loaded
+  if (currentItems >= elementList.length) {
+    event.target.style.display = "none";
+  }
 });
